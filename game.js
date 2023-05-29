ab20aa305961289d80ad65cc9cc32eb2b13ef9c0
Evan Ntinas <77255186+EvanNtinas@users.noreply.github.com>
2023-05-29 17:44:56 +0300

    Add files via upload

---
+++ game.js
@@ -0,0 +1,93 @@
+var buttonColours = ['red', 'blue', 'green', 'yellow']
+
+var gamePattern = []
+
+var userClickedPattern = []
+
+var started = false
+
+var level = 0
+
+// Changes the text when the game starts e.g level 1, level 2 etc
+$(document).keypress(function () {
+  if (!started) {
+    $('#level-title').text('Level' + level)
+    nextSequence()
+    started = true
+  }
+})
+
+//Button clicks
+$('.btn').click(function () {
+  var userChosenColour = $(this).attr('id')
+
+  userClickedPattern.push(userChosenColour)
+
+  playSound(userChosenColour)
+
+  animatePress(userChosenColour)
+
+  checkAnswer(userClickedPattern.length - 1)
+})
+
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function () {
+        nextSequence()
+      }, 1000)
+    }
+  } else {
+    playSound('wrong')
+
+    $('body').addClass('game-over')
+    $('#level-title').text('Game Over, Press Any Key to Restart')
+
+    setTimeout(function () {
+      $('body').removeClass('game-over')
+    }, 200)
+
+    startOver()
+  }
+}
+
+//main function
+function nextSequence() {
+  userClickedPattern = []
+
+  level++
+
+  $('#level-title').text('Level ' + level)
+
+  var randomNumber = Math.floor(Math.random() * 4)
+  var randomChosenColour = buttonColours[randomNumber]
+  gamePattern.push(randomChosenColour)
+
+  $('#' + randomChosenColour)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100)
+
+  playSound(randomChosenColour)
+}
+
+//Change the animation for each clicked button
+function animatePress(currentColour) {
+  $('#' + currentColour).addClass('pressed')
+
+  setTimeout(function () {
+    $('#' + currentColour).removeClass('pressed')
+  }, 100)
+}
+
+// Play sounds after clicking buttons
+function playSound(name) {
+  var audio = new Audio('sounds/' + name + '.mp3')
+  audio.play()
+}
+
+function startOver() {
+  level = 0
+  gamePattern = []
+  started = false
+}
